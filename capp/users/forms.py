f8c383aa1f0f64afb30c46978134709886d21b8d
Nicol <123992698+NCarrascoAngulo@users.noreply.github.com>
2024-12-09 07:19:33 -0500

    Add files via upload

--- capp/users/forms.py
+++ capp/users/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from capp.models import User
+
 
 class RegistrationForm(FlaskForm):
   username = StringField('Username', 
@@ -11,6 +13,16 @@ class RegistrationForm(FlaskForm):
   confirm_password = PasswordField('Confirm Password',
                       validators=[DataRequired(), EqualTo('password')])                                
   submit = SubmitField('Sign Up')
+
+  def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Ese nombre de usuario ya está en uso. Elija uno diferente.')
+
+  def validate_email(self, email):
+      user = User.query.filter_by(email=email.data).first()
+      if user:
+          raise ValidationError('Ese correo ya está en uso. Elija uno diferente.')
 
 class LoginForm(FlaskForm):
   email = StringField('Email',
